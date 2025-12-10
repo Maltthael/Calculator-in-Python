9a85f250782969aeb55090f50126edc75c5c3664
Maltthael <macrodrigues01@hotmail.com>
2025-12-10 18:08:27 -0300

    Adding comments to the code

--- app.py
+++ app.py
@@ -7,10 +7,13 @@ print('*********************************')
 
 def calculator_lambda():
 
-    try:
-        x = float(input('Digite o primeiro numero: '))   
-        operator = input('Digite a operação matemática: ')
-        y = float(input('Digite o segundo numero: '))   
+    try: #Handle the problem input errors
+       
+        x = float(input('Digite o primeiro numero: '))  #Receive the first number
+        operator = input('Digite a operação matemática: ')    #Receive the wished operator
+        y = float(input('Digite o segundo numero: '))  #Receive the second number  
+        
+        #Map each operator to its corresponding calculation
         operations = {
             '+': lambda x, y: x + y, 
             '-': lambda x, y: x - y,
@@ -18,18 +21,19 @@ def calculator_lambda():
             '/': lambda x, y: x / y
         }
         
-        if operator in operations:
-            result = operations[operator](x,y)
-            print(f'Resultado: {result}')
-        elif  y == 0 and operator == '/':
+        #conditions to check if there's any problem and execute the calculation
+        if  operator == '/' and y == 0:
             print('Erro: Divisão com zero não é permitida.')
             raise ZeroDivisionError
+        elif operator in operations:
+            result = operations[operator](x,y)
+            print(f'Resultado: {result}')
         else:
             print('Operador invalido')
             return
-    except ValueError:
+    except ValueError: #If the user type anything diffrent from numbers
         print('Erro: entrada apenas de numeros é permitida.')
-    except ZeroDivisionError:
+    except ZeroDivisionError: # Raised when dividing by zero
         print('Erro: Divisão com zero não é possivel.')
    
    
