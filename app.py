f5bfed2aa654b3b72ed1dd69cf330c33969ccec8
Maltthael <macrodrigues01@hotmail.com>
2025-12-10 16:49:48 -0300

    Adding the lambda calculator

--- app.py
+++ app.py
@@ -1,3 +1,28 @@
 print('*********************************')
 print('Bem vindo a calculadora master !')
 print('*********************************')
+
+x = float(input('Digite o primeiro numero:'))   
+operator = input('Digite a operação matemática: ')
+y = float(input('Digite o segundo numero:'))   
+
+
+def calculator_lambda():
+
+
+      
+        operations = {
+            '+': lambda x, y: x + y, 
+            '-': lambda x, y: x - y,
+            '*': lambda x, y: x * y,
+            '/': lambda x, y: x / y
+        }
+        
+        if operator in operations:
+            result = operations[operator](x,y)
+            print(f'Resultado: {result}')
+        else:
+            print('Operador invalido')
+   
+
+calculator_lambda()
