6cd721a36cab3959ad570b62217edc1ccb6c9026
Maltthael <macrodrigues01@hotmail.com>
2025-12-10 17:03:41 -0300

    Adding the ValueError

--- app.py
+++ app.py
@@ -2,15 +2,15 @@ print('*********************************')
 print('Bem vindo a calculadora master !')
 print('*********************************')
 
-x = float(input('Digite o primeiro numero:'))   
-operator = input('Digite a operação matemática: ')
-y = float(input('Digite o segundo numero:'))   
+
 
 
 def calculator_lambda():
 
-
-      
+    try:
+        x = float(input('Digite o primeiro numero:'))   
+        operator = input('Digite a operação matemática: ')
+        y = float(input('Digite o segundo numero:'))   
         operations = {
             '+': lambda x, y: x + y, 
             '-': lambda x, y: x - y,
@@ -21,8 +21,13 @@ def calculator_lambda():
         if operator in operations:
             result = operations[operator](x,y)
             print(f'Resultado: {result}')
+        elif x == 0 or y == 0 and operations == '/':
+            print('Erro: Divisão com zero não é permitida.')
         else:
             print('Operador invalido')
+    except ValueError:
+        print('Erro: entrada apenas de numeros é permitida.')
+   
    
 
 calculator_lambda()
